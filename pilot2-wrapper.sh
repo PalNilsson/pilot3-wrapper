0fea5d7a0c538aa2816c5894549dbc98a4f288ed
Mario Lassnig <mario@lassnig.net>
2017-01-24 12:31:37 +0100

    add configuration, pilot download, pilot execution

--- pilot2-wrapper.sh
+++ pilot2-wrapper.sh
@@ -3,17 +3,18 @@
 # wrapper for pilot2
 # author: mario.lassnig@cern.ch
 
-VERSION=20161027.001
+VERSION=20170124.001
 
 function log_es() {
-    curl -ks --connect-timeout 5 --max-time 10 --netrc -XPOST https://es-atlas.cern.ch:9203/atlas_pilotfactory-$(date --utc +"%Y-%m-%d")/event/ -d \
-	 '{"timestamp": "'$(date --utc +%Y-%m-%dT%H:%M:%S.%3N)'",
-           "apffid": "'$APFFID'",
-           "apfcid": "'$APFCID'",
-           "host": "'$(hostname -f)'",
-           "pid": "'$$'",
-           "version": "'$VERSION'",
-           "msg": "'"$@"'"}' 1>/dev/null;
+    # curl -ks --connect-timeout 5 --max-time 10 --netrc -XPOST https://es-atlas.cern.ch:9203/atlas_pilotfactory-$(date --utc +"%Y-%m-%d")/event/ -d \
+    # 	 '{"timestamp": "'$(date --utc +%Y-%m-%dT%H:%M:%S.%3N)'",
+    #        "apffid": "'$APFFID'",
+    #        "apfcid": "'$APFCID'",
+    #        "host": "'$(hostname -f)'",
+    #        "pid": "'$$'",
+    #        "version": "'$VERSION'",
+    #        "msg": "'"$@"'"}' 1>/dev/null;
+    sleep 0.01
 }
 
 function log_stdout() {
@@ -50,8 +51,33 @@ function trap_handler() {
 function main() {
     apfmon_start
     log_stdout "pilot2 wrapper version=$VERSION apffid=$APFFID apfcid=$APFCID"
+
     log_stdout "support: atlas-adc-pilot@cern.ch"
     log_stdout "author: mario.lassnig@cern.ch"
+
+    log_stdout "--- parsing arguments ---"
+
+    while getopts ":s:q:" opt; do
+        case $opt in
+            s)
+                configured_site=$OPTARG
+                ;;
+            q)
+                configured_queue=$OPTARG
+                ;;
+            \?)
+                log_stdout "Unused option: $OPTARG" >&2
+                ;;
+        esac
+    done
+
+    if [ -z $configured_site ] || [ -z $configured_queue ]; then
+        log_stdouterr "both site (-s) and queue (-q) must be specified"
+        log_stdouterr "aborting"
+        exit 1
+    fi
+    log_stdout "Site: $configured_site"
+    log_stdout "Queue: $configured_queue"
 
     log_stdout "--- main ---"
     log_es "main"
@@ -63,7 +89,7 @@ function main() {
     log_stdout "whoami: $(whoami)"
     log_stdout "id: $(id)"
     if [[ -r /proc/version ]]; then
-	log_stdout "/proc/version: $(cat /proc/version)"
+        log_stdout "/proc/version: $(cat /proc/version)"
     fi
     log_stdout "ulimit: \n$(ulimit -a)"
     log_stdout "env: \n$(printenv | sort)"
@@ -72,10 +98,10 @@ function main() {
     log_es "proxy"
     out=$( { voms-proxy-info --all; } 2>&1)
     if [ $? -ne 0 ]; then
-	out=$(echo ${out} | tr -d '\n')
-	log_stdouterr "failed: voms-proxy-info --all: $out"
-	log_stdouterr "aborting"
-	exit 1
+        out=$(echo ${out} | tr -d '\n')
+        log_stdouterr "failed: voms-proxy-info --all: $out"
+        log_stdouterr "aborting"
+        exit 1
     fi
     log_stdout "voms-proxy-info: \n$out"
 
@@ -83,14 +109,14 @@ function main() {
     log_es "setup working directory"
     init_dir=$(pwd)
     work_dir_template=$(pwd)/condorg_XXXXXXXX
-    work_dir=$( { mktemp -d $(work_dir_template); } 2>&1)
+    work_dir=$( { mktemp -d $work_dir_template; } 2>&1)
     if [ $? -ne 0 ]; then
-	log_stdouterr "failed: $work_dir"
-	log_stdouterr "aborting"
-	exit 1
+        log_stdouterr "failed: $work_dir"
+        log_stdouterr "aborting"
+        exit 1
     else
-	cd $work_dir
-	log_stdout "pwd: $(pwd)"
+        cd $work_dir
+        log_stdout "pwd: $(pwd)"
     fi
 
     log_stdout "--- setup ALRB ---"
@@ -106,6 +132,8 @@ function main() {
 
     log_stdout "--- retrieving pilot ---"
     log_es "retrieving pilot"
+    wget -q https://github.com/mlassnig/pilot2/archive/ongoing-work.tar.gz -O pilot.tar.gz
+    tar xfz pilot.tar.gz --strip-components=1
 
     log_stdout "--- installing signal handler ---"
     log_es "installing signal handler"
@@ -113,6 +141,7 @@ function main() {
 
     log_stdout "--- running pilot ---"
     log_es "running pilot"
+    python pilot.py -d -s $configured_site -q $configured_queue
 
     log_stdout "--- cleanup ---"
     log_es "cleanup"
