5f88691878496c381ab5119f54ccdb4616c40d3d
Paul Nilsson <palnilsson70@gmail.com>
2021-01-26 12:28:41 +0100

    Cleanup - removed APF monitoring

--- pilot3-wrapper.sh
+++ pilot3-wrapper.sh
@@ -4,20 +4,7 @@
 # author: paul.nilsson@cern.ch
 # ./pilot3-wrapper.sh -w generic -a /scratch -j ptest -q UTA_PAUL_TEST -v https://aipanda007.cern.ch -l 2000
 
-VERSION=20210125.001
-
-function log_es() {
-    if [ ! -z ${APFMON+x} ] && [ ! -z ${APFFID+x} ] && [ ! -z ${APFCID+x} ]; then
-        curl -ks --connect-timeout 5 --max-time 10 --netrc -XPOST https://es-atlas.cern.ch:9203/atlas_pilotfactory-$(date --utc +"%Y-%m-%d")/event/ -d \
-	     '{"timestamp": "'$(date --utc +%Y-%m-%dT%H:%M:%S.%3N)'",
-           "apffid": "'$APFFID'",
-           "apfcid": "'$APFCID'",
-           "host": "'$(hostname -f)'",
-           "pid": "'$$'",
-           "version": "'$VERSION'",
-           "msg": "'"$@"'"}' 1>/dev/null;
-    fi
-}
+VERSION=20210126.001
 
 function log_stdout() {
     date --utc +"%Y-%m-%d %H:%M:%S %Z [wrapper:stdout] " | tr -d '\n'
@@ -34,14 +21,6 @@ function log_stdouterr() {
     echo -e "$@"
     date --utc +"%Y-%m-%d %H:%M:%S %Z [wrapper:stderr] " | tr -d '\n' >&2
     echo -e "$@" >&2
-}
-
-function apfmon_start() {
-    curl -ks --connect-timeout 10 --max-time 20 -d state=running -d wrapper=$VERSION ${APFMON}/jobs/${APFFID}:${APFCID} 1>/dev/null
-}
-
-function apfmon_end() {
-    curl -ks --connect-timeout 10 --max-time 20 -d state=exiting -d rc=$1 ${APFMON}/jobs/${APFFID}:${APFCID} 1>/dev/null
 }
 
 function trap_handler() {
@@ -71,13 +50,7 @@ function show_help() {
 }
 function main() {
 
-    if [ ! -z ${APFMON+x} ] && [ ! -z ${APFFID+x} ] && [ ! -z ${APFCID+x} ]; then
-        #apfmon_start
-        log_stdout "pilot3 wrapper version=$VERSION apffid=$APFFID apfcid=$APFCID"
-    else
-        log_stdout "pilot3 wrapper version=$VERSION"
-    fi
-
+    log_stdout "pilot3 wrapper version=$VERSION"
     log_stdout "support: atlas-adc-pilot@cern.ch"
     log_stdout "author: paul.nilsson@cern.ch"
 
@@ -161,11 +134,7 @@ function main() {
     # Run the OSG setup if necessary
     setup_osg
 
-    log_stdout "--- main ---"
-    #log_es "main"
-
     log_stdout "--- environment ---"
-    #log_es "environment"
     log_stdout "hostname: $(hostname -f)"
     log_stdout "pwd: $(pwd)"
     log_stdout "whoami: $(whoami)"
@@ -173,11 +142,9 @@ function main() {
     if [[ -r /proc/version ]]; then
         log_stdout "/proc/version: $(cat /proc/version)"
     fi
-    log_stdout "ulimit: \n$(ulimit -a)"
     log_stdout "env: \n$(printenv | sort)"
 
     log_stdout "--- proxy ---"
-    #log_es "proxy"
     out=$( { voms-proxy-info --all; } 2>&1)
     if [ $? -ne 0 ]; then
         out=$(echo ${out} | tr -d '\n')
@@ -188,7 +155,6 @@ function main() {
     log_stdout "voms-proxy-info: \n$out"
 
     log_stdout "--- setup working directory ---"
-    #log_es "setup working directory"
     init_dir=$(pwd)
     work_dir_template=$(pwd)/condorg_XXXXXXXX
     work_dir=$( { mktemp -d $work_dir_template; } 2>&1)
@@ -202,7 +168,6 @@ function main() {
     fi
 
     #log_stdout "--- setup ALRB ---"
-    #log_es "setup ALRB"
     #export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
     #source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh --quiet
     #source $VO_ATLAS_SW_DIR/local/setup.sh -s $queue
@@ -211,40 +176,30 @@ function main() {
     #lsetup "python 2.7.9-x86_64-slc6-gcc48"
 
     #log_stdout "--- setup DDM ---"
-    #log_es "setup DDM"
     #export VO_LOCAL_QUEUE=$queue
     #source $ATLAS_LOCAL_ROOT_BASE/utilities/oldAliasSetup.sh "rucio testing-SL6"
     #log_stdout "rucio whoami: \n$(rucio whoami)"
 
     log_stdout "--- retrieving pilot ---"
-    #log_es "retrieving pilot"
     wget -q http://cern.ch/atlas-panda-pilot/pilot2-dev2.tar.gz
     tar xfz pilot2-dev2.tar.gz --strip-components=1
 
     log_stdout "--- installing signal handler ---"
-    #log_es "installing signal handler"
     trap trap_handler SIGTERM SIGQUIT SIGSEGV SIGXCPU SIGUSR1 SIGBUS
 
     log_stdout "--- running pilot ---"
-    #log_es "running pilot"
 
     echo python3 pilot.py $proxy $debug -a $workdir -j $job_label -w $workflow -q $queue --pilot-user=$pilot_user --url=$url $lifetime_arg $hpc_arg
     python3 pilot.py $proxy $debug -a $workdir -j $job_label -w $workflow -q $queue --pilot-user=$pilot_user --url=$url $lifetime_arg $hpc_arg
     ec=$?
     log_stdout "exitcode: $ec"
 
-    #if [ ! -z ${APFMON+x} ] && [ ! -z ${APFFID+x} ] && [ ! -z ${APFCID+x} ]; then
-    #    apfmon_end $ec
-    #fi
-
     log_stdout "--- cleanup ---"
-    #log_es "cleanup"
 
     cd $init_dir
     rm -rf $work_dir
 
     log_stdout "--- done ---"
-    #log_es "done"
 }
 
 main "$@"
